91e76cbbf13db2c3a5cd1a2033823cc7056e6a95
Yash Patel <yp0034065@gmail.com>
2026-07-03 10:58:04 +0530

    Cart functionality: Add 2 products to cart

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -1,6 +1,11 @@
 import { expect, Page } from "@playwright/test";
 
 export class InventoryPage {
+  async addProductsToCart(page: Page) {
+    await page.locator("#add-to-cart-sauce-labs-backpack").click();
+    await page.locator("#add-to-cart-sauce-labs-bike-light").click();
+  }
+
   async logoutUser(page: Page) {
     await page.locator("#react-burger-menu-btn").click();
     await page.locator("#logout_sidebar_link").click();
---
+++ tests/cart.spec.ts
@@ -0,0 +1,13 @@
+import test from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+import { users } from "../utils/testData";
+import { InventoryPage } from "../pages/InventoryPage";
+
+const login = new LoginPage();
+const inventoary = new InventoryPage();
+
+test("Cart functionality", async ({ page }) => {
+  await login.open(page);
+  await login.login(page, users.valid_users, users.password);
+  await inventoary.addProductsToCart(page);
+});
